ea6c218fb5780882a9fbefb445662d23a21a13bb
sohel5G <105277301+sohel5G@users.noreply.github.com>
2024-01-13 23:42:18 +0600

    Data type

--- variable.php
+++ variable.php
@@ -1,15 +1,63 @@
 <?php
 
-$name  = 'Bangladesh';
-$age = '55 Years';
+// $name  = 'Bangladesh';
+// $age = '55 Years';
 
-$word = 'age' ;
+// $word = 'age' ;
 
 // echo $word;
 // echo "\n";
 // echo $$word; 
 
-echo "Hello " . $name;
+// echo "Hello " . $name;
+// echo "\n";
+// echo "Hello $name";
+// echo "\n";
+// echo "Hello {$name}";
 
 
+
+
+
+// $task = "Read";
+
+// // echo $task;
+
+// $task = "Write";
+
+// echo $task;
+// echo "\n";
+
+// define("myName", "Sohel Rana");
+
+// echo myName;
+
+
+
+
+$name = "Earth";
+$age = 99999;
+$float = 2.34;
+$null = null;
+
+var_dump($name);
+var_dump($age);
+var_dump($float);
+var_dump($null);
+
+
+/* 
+
+Integer Int
+double/float
+Boolean 
+Null
+String
+Array
+Object
+
+
+
+
+*/
 
