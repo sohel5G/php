195663d86f18b2d3faaca41cfd45872251e30ecb
sohel5G <105277301+sohel5G@users.noreply.github.com>
2024-01-29 20:42:14 +0600

    array

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+
+$arrName = [2, 5, "abc", 7, 98];
+
+// var_dump( $arrName );
+
+echo count($arrName);
+
---
+++ scope.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Abdullah Al Araf";
+
+function fncName(){
+
+    // global $name;
+    // echo $name;
+
+    // return $GLOBALS['name'];
+}
+
+echo fncName()
+
+
+
+?>
