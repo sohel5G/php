cfd2e081ca3490b7c1dea9b6decd6072703ba51d
sohel5G <105277301+sohel5G@users.noreply.github.com>
2024-02-08 12:33:58 +0600

    Accocialtive array

--- array.php
+++ array.php
@@ -1,8 +1,30 @@
 <?php
 
-$arrName = [2, 5, "abc", 7, 98];
+// $arrName = [2, 5, "abc", 7, 98];
 
-// var_dump( $arrName );
+// // var_dump( $arrName );
 
-echo count($arrName);
+// echo count($arrName);
 
+
+
+// ৩.২ - অ্যারে ম্যানিপুলেশন
+
+$students = ['Araf', 'Arif'. 'Jamal', 'Kmal', 'Mosthofa'];
+
+$student = array_pop($students);
+// array_shift();
+// array_unshift();
+// array_push();
+
+echo ($student);
+
+// Associative array 
+
+$foods = [
+    'drink' => 'water, milk, mango juce',
+    'vegitable' => 'lau, begun, flower'
+];
+
+
+echo( $foods['drink'] );
